6fc1a9578fb9aa715bac3366337de2e9e4d1393c
nwachukwusamuel2503 <santosfourreal@gmail.com>
2023-06-16 14:03:45 -0700

    Task Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,17 +11,17 @@ int main(void)
 	int i = '0';
 	int j = '0';
 
-	while(i <= '9')
+	while (i <= '9')
 	{
 		while (j <= '9')
 		{
-			if(!(i > j) || i == j)
+			if (!(i > j) || i == j)
 			{
 				putchar(i);
 				putchar(j);
 				if (i == '8' && j == '9')
 				{
-					putchar('\n');		
+					putchar('\n');
 				}
 				else
 				{
