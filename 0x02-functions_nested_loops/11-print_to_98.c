f33c545bdca0b1648f5a0eb9298b0b108219c62f
nwachukwusamuel2503 <santosfourreal@gmail.com>
2023-06-20 05:57:05 -0700

    Task 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,13 +2,13 @@
 	#include "main.h"
 
 
-	/**
-	 * print_to_98 - prints all natural numbers from n to 98,
-	 * followed by a new line
-	 * @n: print from this number
-	 */
-	void print_to_98(int n)
-	{
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
 		int i, j;
 
 
@@ -31,4 +31,4 @@
 					printf("%d\n", j);
 			}
 		}
-	}
+}
