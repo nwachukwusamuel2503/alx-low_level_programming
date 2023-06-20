451beb7cab1d61032cf76d11b20572560c457fcc
nwachukwusamuel2503 <santosfourreal@gmail.com>
2023-06-20 06:28:09 -0700

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
+/**
+ * main - print _putchar
+ * is written by samuel
+ * Return: 0
+ */
 
-	int main(void)
-	{
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,4 +17,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
