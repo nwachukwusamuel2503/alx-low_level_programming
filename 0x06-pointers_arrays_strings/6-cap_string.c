03c6aa54b626c83584eec432929e5509d0fd702d
nwachukwusamuel2503 <santosfourreal@gmail.com>
2023-06-29 05:06:09 -0700

    Task 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
 *cap_string - function that capitalize first character of a word
 *@str: string to capitalize
 *Return:returns the capitalized string
 */
+
 char *cap_string(char *str)
 {
-int index = 0;
+	int index = 0;
 
 	while (str[++index])
 	{
@@ -27,5 +29,6 @@ int index = 0;
 				str[index - 1] == '{' ||
 				str[index - 1] == '}')
 			str[index] -= 32;
+	}
 	return (str);
 }
